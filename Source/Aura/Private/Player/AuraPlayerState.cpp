643f521591df404c2f8b3c4f1d94d1ebbb4da2cd
TplusGames <134178294+TplusGames@users.noreply.github.com>
2025-04-24 13:42:43 -0500

    Aura Attribute Set and ASC

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	//Set net update frequency to higher value for player ASC
+	NetUpdateFrequency = 100.f;
+	
+}
